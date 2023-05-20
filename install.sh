8c7635254eb58928307ac82f6eb4f68704d4ede7
RopFoo <ropfoo@gmail.com>
2023-05-20 15:50:37 +0200

    add default yml to install

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 curl -L https://github.com/ropfoo/clobbopus/releases/download/Latest/clobbopus_data --output clobbopus_data
 curl -L https://github.com/ropfoo/clobbopus/releases/download/Latest/clobbopus_server --output clobbopus_server
+curl -L https://github.com/ropfoo/clobbopus/releases/download/Latest/clobbopus.yml --output clobbopus.yml
 
 chmod +x clobbopus_server
 chmod +x clobbopus_data
